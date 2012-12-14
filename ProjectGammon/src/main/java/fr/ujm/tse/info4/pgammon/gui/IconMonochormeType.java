398c2cd94dfe2b87447b5abb924460372db9f6cf
POTHELUNE Jean-Michel <pothelune.jean-michel@telecom-st-etienne.fr>
2012-12-14 15:04:23 +0100

    Ajout des icones et leurs liens
    Liens créés dans IconMonochromeType
    
    Signed-off-by: POTHELUNE Jean-Michel <pothelune.jean-michel@telecom-st-etienne.fr>

---
+++ ProjectGammon/src/main/java/fr/ujm/tse/info4/pgammon/gui/IconMonochormeType.java
@@ -0,0 +1,12 @@
+package fr.ujm.tse.info4.pgammon.gui;
+
+public class IconMonochormeType {
+	public static final String RETOUR 	= "iamges/back.png";
+	public static final String DES 		= "iamges/dices.png";
+	public static final String AIDE 	= "iamges/help.png";
+	public static final String SUIVANT 	= "iamges/next.png";
+	public static final String PLUS 	= "iamges/plus.png";
+	public static final String ANNULER 	= "iamges/undo.png";
+	public static final String X_NOIR 	= "iamges/x_black.png";
+	public static final String X_BLANC 	= "iamges/x_white.png";
+}
