0036694032b8b6fb1639ee9304251b84e4fdd96b
POTHELUNE Jean-Michel <pothelune.jean-michel@telecom-st-etienne.fr>
2012-12-15 06:50:08 +0100

    Netoyage classe MonochromeButton
    Signed-off-by: POTHELUNE Jean-Michel <pothelune.jean-michel@telecom-st-etienne.fr>

--- ProjectGammon/src/main/java/fr/ujm/tse/info4/pgammon/gui/MonochromeButton.java
+++ ProjectGammon/src/main/java/fr/ujm/tse/info4/pgammon/gui/MonochromeButton.java
@@ -59,56 +59,43 @@ public class MonochromeButton extends JButton {
 	}
 	@Override
 	protected void paintComponent(Graphics g) {
-		super.paintComponent(g); 
+		
 		Graphics2D g2 = (Graphics2D) g.create(); 
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); 
 		
 		int h = getHeight(); 
 		int w = getWidth(); 
 		
-		
+		Paint p;
 		ButtonModel model = getModel(); 
 
-		Paint p;
 		
+		// Fond
 		if (!model.isEnabled()) { 
 			setForeground(Color.GRAY);
 			p = new Color(0x444444);
-		}else{ 
+		}
+		else { 
 			if (model.isRollover()) { 
 				p = new Color(0x333333);
-
 			} else { 
 				p = new Color(0x222222);
-				
 			} 
 		} 
 		g2.setPaint(p); 
-		
-		Paint p1; 
-		Paint p2; 
-		if (model.isPressed()) { 
-			p1 = new Color(0x888888);
-			p2 = new Color(0x555555);
-		} else { 
-			p1 = new Color(0x555555);
-			p2 = new Color(0x888888);
-		} 
-		Rectangle2D.Float r2d = new Rectangle2D.Float(0, 0, w - 1, h - 1); 
-		Shape clip = g2.getClip(); 
-		g2.clip(r2d); 
 		g2.fillRect(0, 0, w, h); 
-		
-		g2.setClip(clip); 
-		
+
+		 
+		// Bordure
+		if (model.isPressed()) { 
+			p = new Color(0x555555);
+		} else { 
+			p = new Color(0x888888);
+		} 
 		g2.setStroke(new BasicStroke(5.0f) );
-		
-		g2.setPaint(p1); 
-		g2.drawRect(0, 0, w - 1, h - 1);
-		g2.setPaint(p2); 
+		g2.setPaint(p); 
 		g2.drawRect(1, 1, w - 3, h - 3);
 			
-		//
 		g2.dispose(); 
 		
 	}
