342b32bc02598e6fd7dc67343410144ffd4bf1b9
POTHELUNE Jean-Michel <pothelune.jean-michel@telecom-st-etienne.fr>
2012-12-15 05:30:32 +0100

    Creation de la classe Case
    Signed-off-by: POTHELUNE Jean-Michel <pothelune.jean-michel@telecom-st-etienne.fr>

--- ProjectGammon/src/main/java/fr/ujm/tse/info4/pgammon/models/Case.java
+++ ProjectGammon/src/main/java/fr/ujm/tse/info4/pgammon/models/Case.java
@@ -18,15 +18,51 @@ public class Case
 	private CouleurCase couleurDame;
 	private int nbDame;
 	private int position;
-	public Case(CouleurCase couleur)
+
+	public Case(int position)
 	{
-		//TODO
-		throw new UnsupportedOperationException();
+		this.position = position;
+		this.couleurDame = CouleurCase.VIDE;
+		this.nbDame = 0;
+	}
+	
+	public Case(int position, CouleurCase couleur)
+	{
+		this.position = position;
+		this.couleurDame = couleur;
+		this.nbDame = 0;
+	}
+	
+	public Case(int position, CouleurCase couleur,int nombreDame)
+	{
+		this.position = position;
+		this.couleurDame = couleur;
+		this.nbDame = nombreDame;
 	}
 	
 	public boolean isCaseVictoire()
 	{
 		//TODO
 		throw new UnsupportedOperationException();
+	}
+
+	public CouleurCase getCouleurDame() {
+		return couleurDame;
+	}
+
+	public void setCouleurDame(CouleurCase couleurDame) {
+		this.couleurDame = couleurDame;
+	}
+
+	public int getNbDame() {
+		return nbDame;
+	}
+
+	public void setNbDame(int nbDame) {
+		this.nbDame = nbDame;
+	}
+
+	public int getPosition() {
+		return position;
 	}
 }
