fc09610f318bf4a362db8b36d13762141b2bc5d9
bbonnetto <bbonnetto@gmail.com>
2012-12-17 15:45:04 +0100

    inittialisationTest tablier fonctionnel

--- ProjectGammon/src/main/java/fr/ujm/tse/info4/pgammon/controleur/ControleurPartie.java
+++ ProjectGammon/src/main/java/fr/ujm/tse/info4/pgammon/controleur/ControleurPartie.java
@@ -8,6 +8,10 @@
 //
 
 package fr.ujm.tse.info4.pgammon.controleur;
+import java.util.ArrayList;
+
+import fr.ujm.tse.info4.pgammon.models.Case;
+import fr.ujm.tse.info4.pgammon.models.CouleurCase;
 import fr.ujm.tse.info4.pgammon.models.Partie;
 import fr.ujm.tse.info4.pgammon.vues.VuePartie;
 
@@ -20,7 +24,9 @@ public class ControleurPartie
 	
 	public  ControleurPartie(Partie partie)
 	{
+		
 		this.partie = partie;
+		testInitialisation();
 		vuePartie = new VuePartie(partie);
 		controleurTablier = new ControleurTablier(partie,vuePartie.getVueTablier());
 		build();
@@ -42,5 +48,34 @@ public class ControleurPartie
 		return controleurTablier;
 	}
 	
+	private void testInitialisation(){
+		ArrayList<Case> lCase = new ArrayList<Case>();
+		
+		lCase.add(new Case(CouleurCase.NOIR, 5, 1));
+		lCase.add(new Case(CouleurCase.NOIR, 5, 2));
+		lCase.add(new Case(CouleurCase.NOIR, 5, 3));
+		lCase.add(new Case(CouleurCase.VIDE, 0, 4));
+		lCase.add(new Case(CouleurCase.VIDE, 0, 5));
+		lCase.add(new Case(CouleurCase.VIDE, 0, 6));
+		lCase.add(new Case(CouleurCase.VIDE, 0, 7));
+		lCase.add(new Case(CouleurCase.VIDE, 0, 8));
+		lCase.add(new Case(CouleurCase.VIDE, 0, 9));
+		lCase.add(new Case(CouleurCase.VIDE, 0, 10));
+		lCase.add(new Case(CouleurCase.VIDE, 0, 12));
+		lCase.add(new Case(CouleurCase.VIDE, 0, 13));
+		lCase.add(new Case(CouleurCase.VIDE, 0, 14));
+		lCase.add(new Case(CouleurCase.VIDE, 0, 15));
+		lCase.add(new Case(CouleurCase.VIDE, 0, 16));
+		lCase.add(new Case(CouleurCase.VIDE, 0, 17));
+		lCase.add(new Case(CouleurCase.VIDE, 0, 18));
+		lCase.add(new Case(CouleurCase.VIDE, 0, 19));
+		lCase.add(new Case(CouleurCase.VIDE, 0, 20));
+		lCase.add(new Case(CouleurCase.VIDE, 0, 21));
+		lCase.add(new Case(CouleurCase.BLANC, 5, 22));
+		lCase.add(new Case(CouleurCase.BLANC, 5, 23));
+		lCase.add(new Case(CouleurCase.BLANC, 5, 24));
+
+		partie.getTablier().initialiserCase(lCase);
+	}
 	
 }
