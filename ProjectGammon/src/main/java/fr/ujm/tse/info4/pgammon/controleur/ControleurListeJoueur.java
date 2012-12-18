5b4e0749450e8ac4f0471683c0d1610d87ba1526
bbonnetto <bbonnetto@gmail.com>
2012-12-18 18:05:33 +0100

    modification controleur

--- ProjectGammon/src/main/java/fr/ujm/tse/info4/pgammon/controleur/ControleurListeJoueur.java
+++ ProjectGammon/src/main/java/fr/ujm/tse/info4/pgammon/controleur/ControleurListeJoueur.java
@@ -1,5 +1,7 @@
 package fr.ujm.tse.info4.pgammon.controleur;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.IOException;
 
 import org.jdom2.JDOMException;
@@ -9,12 +11,17 @@ import fr.ujm.tse.info4.pgammon.vues.VueListeJoueur;
 
 public class ControleurListeJoueur {
 
+	private VueListeJoueur vueListeJoueur;
+	private ControleurPrincipal controleurPrincipal;
+	private Profils profil;
+	
 	public ControleurListeJoueur(ControleurPrincipal controleurPrincipal) 
 	{
+		this.controleurPrincipal = controleurPrincipal;
 		//TODO a changer
-		Profils tmp = new Profils();
+		profil = Profils().getProfil();
 		try {
-			tmp.charger();
+			profil.charger();
 		} catch (JDOMException e) {
 			
 			//todo message probleme
@@ -24,13 +31,36 @@ public class ControleurListeJoueur {
 	
 		//
 		
-		VueListeJoueur vueListeJoueur = new VueListeJoueur(tmp);
+		vueListeJoueur = new VueListeJoueur(profil);
 		controleurPrincipal.getFrame().setContentPane(vueListeJoueur);
 		build();
 	}
 	
 	public void build()
 	{
-		
+		listenerBouttonRetour();
+
+	}
+	
+	
+	public void listenerBouttonRetour()
+	{
+		vueListeJoueur.getRetour().addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				vueListeJoueur.setVisible(false);
+				profil.sauvegarder();
+				controleurPrincipal.retourMenu();
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) {}
+			@Override
+			public void mouseExited(MouseEvent e) {}
+			@Override
+			public void mousePressed(MouseEvent e) {}
+			@Override
+			public void mouseReleased(MouseEvent e) {}
+		});
 	}
 }
