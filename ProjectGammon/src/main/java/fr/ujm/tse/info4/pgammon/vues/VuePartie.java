2003c3d1fdf9013e1ac7d90ea912cd60c31c8904
didi <adrien.francon42@gmail.com>
2012-12-18 09:58:44 +0100

    modif vue partie

--- ProjectGammon/src/main/java/fr/ujm/tse/info4/pgammon/vues/VuePartie.java
+++ ProjectGammon/src/main/java/fr/ujm/tse/info4/pgammon/vues/VuePartie.java
@@ -32,17 +32,12 @@ public class VuePartie extends JPanel{
 	
 	
 	
-	//composante panel bas
-	MonochromeIconButton help;
-	MonochromeIconButton plus;
-	MonochromeIconButton x_black;
-	MonochromeButton scorej1;
-	MonochromeButton scorej2;
-	
 	
 	private PanelTermineVueDroite paneldroitrevoir;
 	
 	private PanelEnCoursVueDroite paneldroitencours;
+	
+	private PanelEnCoursVueBas panelEnCoursVueBas;
 	
 	
 	public VuePartie(Partie partie) {
@@ -58,84 +53,59 @@ public class VuePartie extends JPanel{
 		setLayout(null);
 		vueTablier.setBounds(173, 30, 547, 446);
 		add(vueTablier);
+		
+		//
+		//
+		//
 		etat=EtatSession.EN_COURS;
 		
-	
+		
+		//creation des panels de la partie
+		paneldroitencours = new PanelEnCoursVueDroite();
+		paneldroitencours.setBounds(720,0,80,476);
+		add(paneldroitencours);
+		
+		panelEnCoursVueBas = new PanelEnCoursVueBas();
+		panelEnCoursVueBas.setBounds(0,505,800,95);
+		add(panelEnCoursVueBas);
+		
+		paneldroitrevoir = new PanelTermineVueDroite();
+		paneldroitrevoir.setBounds(720,0,80,476);
+		add(paneldroitrevoir);
 		
 		
+		
+		//changement des panels en fonctionde l'etat
 		if(etat.equals(EtatSession.EN_COURS)){
 			
-			paneldroitencours = new PanelEnCoursVueDroite();
-			paneldroitencours.setBounds(720,0,80,476);
-			add(paneldroitencours);
-		
-		
-		//
-		//panel du bas
-		//
-		
-		JPanel conteneurboutonbasjouerpartie = new JPanel();
-		conteneurboutonbasjouerpartie.setLayout(null);
-		conteneurboutonbasjouerpartie.setBounds(0,505,800,95);
-		conteneurboutonbasjouerpartie.setOpaque(false);
-		add(conteneurboutonbasjouerpartie);
-		
-		//composant et label d'aide
-		help = new MonochromeIconButton(IconMonochromeType.AIDE,"MonochromeIconButton","NOIR");
-		help.setSizeBig();
-		help.setBounds(600, 5, help.getPreferredSize().width, help.getPreferredSize().height);
-		conteneurboutonbasjouerpartie.add(help);
-		
-		JLabel labhelp = new JLabel("Aide");
-		labhelp.setForeground(new Color(0xCCCCCC));
-		labhelp.setBounds(617, 50, 80, 60);
-		conteneurboutonbasjouerpartie.add(labhelp);
-		
-		//composant et label d'annuler nouvelle session
-		plus = new MonochromeIconButton(IconMonochromeType.PLUS,"MonochromeIconButton","NOIR");
-		plus.setSizeBig();
-		plus.setBounds(700, 5, plus.getPreferredSize().width, plus.getPreferredSize().height);
-		conteneurboutonbasjouerpartie.add(plus);
-		
-		JLabel labplus = new JLabel("Nouvelle session");
-		labplus.setForeground(new Color(0xCCCCCC));
-		labplus.setBounds(680, 50, 130, 60);
-		conteneurboutonbasjouerpartie.add(labplus);
-		
-		//composant et label d'annuler interrompre session
-		x_black = new MonochromeIconButton(IconMonochromeType.X_NOIR,"MonochromeIconButton","NOIR");
-		x_black.setSizeBig();
-		x_black.setBounds(500, 5, x_black.getPreferredSize().width, x_black.getPreferredSize().height);
-		conteneurboutonbasjouerpartie.add(x_black);
-		
-		JLabel labx_black = new JLabel("Interrompre session");
-		labx_black.setForeground(new Color(0xCCCCCC));
-		labx_black.setBounds(470, 50, 130, 60);
-		conteneurboutonbasjouerpartie.add(labx_black);
-		
-		
-		//composant et label du score
-		scorej1 = new MonochromeButton("4");
-		scorej1.setBounds(270, 5, x_black.getPreferredSize().width, x_black.getPreferredSize().height);
-		conteneurboutonbasjouerpartie.add(scorej1);
-		
-		scorej2 = new MonochromeButton("3");
-		scorej2.setBounds(350, 5, x_black.getPreferredSize().width, x_black.getPreferredSize().height);
-		conteneurboutonbasjouerpartie.add(scorej2);
-		
-		JLabel labscore = new JLabel("Scores");
-		labscore.setForeground(new Color(0xCCCCCC));
-		labscore.setBounds(320, 50, 130, 60);
-		conteneurboutonbasjouerpartie.add(labscore);
+			paneldroitencours.setVisible(true);
+			paneldroitrevoir.setVisible(false);
 		
 		}
 		
 		else if(etat.equals(EtatSession.TERMINEE)){
 		
-			paneldroitrevoir = new PanelTermineVueDroite();
-			paneldroitrevoir.setBounds(720,0,80,476);
-			add(paneldroitrevoir);
+			paneldroitencours.setVisible(false);
+			paneldroitrevoir.setVisible(true);
 			
+		}
+		
+etat=EtatSession.EN_COURS;
+		
+	
+		
+		
+		if(etat.equals(EtatSession.EN_COURS)){
+				
+			paneldroitencours.setVisible(true);
+			paneldroitrevoir.setVisible(false);
+		
+		}
+		
+		else if(etat.equals(EtatSession.TERMINEE)){
+		
+			paneldroitencours.setVisible(false);
+			paneldroitrevoir.setVisible(true);
 		}
 	}
 	
@@ -143,21 +113,6 @@ public class VuePartie extends JPanel{
 	
 	
 
-	public MonochromeButton getScorej1() {
-		return scorej1;
-	}
-
-	public void setScorej1(MonochromeButton scorej1) {
-		this.scorej1 = scorej1;
-	}
-
-	public MonochromeButton getScorej2() {
-		return scorej2;
-	}
-
-	public void setScorej2(MonochromeButton scorej2) {
-		this.scorej2 = scorej2;
-	}
 
 	public Partie getPartie() {
 		return partie;
@@ -167,18 +122,6 @@ public class VuePartie extends JPanel{
 		return etat;
 	}
 
-	public MonochromeIconButton getHelp() {
-		return help;
-	}
-
-	public MonochromeIconButton getPlus() {
-		return plus;
-	}
-
-	public MonochromeIconButton getX_black() {
-		return x_black;
-	}
-
 
 	public VueTablier getVueTablier() {
 		return vueTablier;
@@ -186,7 +129,10 @@ public class VuePartie extends JPanel{
 	
 	
 	
-	
+	public PanelEnCoursVueBas getPanelEnCoursVueBas() {
+		return panelEnCoursVueBas;
+	}
+
 	public PanelEnCoursVueDroite getPaneldroitencours() {
 		return paneldroitencours;
 	}
