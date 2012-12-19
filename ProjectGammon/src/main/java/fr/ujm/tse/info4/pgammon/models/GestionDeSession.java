815396f2e1aab80ed908afd22ad910314e13bac6
Dong Chuan <dongchuan55@gmail.com>
2012-12-19 16:55:51 +0100

    Creation de sauvegarder de Session

--- ProjectGammon/src/main/java/fr/ujm/tse/info4/pgammon/models/GestionDeSession.java
+++ ProjectGammon/src/main/java/fr/ujm/tse/info4/pgammon/models/GestionDeSession.java
@@ -1,8 +1,14 @@
 package fr.ujm.tse.info4.pgammon.models;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 
 public class GestionDeSession {
 	public List<Session> listSession = new ArrayList<Session>();
@@ -23,6 +29,19 @@ public class GestionDeSession {
 	
 	public void sauvegarder(){
 		
+		for(int i=0;i<listSession.size();i++){
+	        try{
+	        	String tmpNom = "Session"+String.valueOf(i);
+				Element racine = new Element(tmpNom);
+				Document document = new Document(racine);
+				//listSession.get(i).sauvegarder(racine);
+				XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
+				String tmpPath = "sauvegarde/"+tmpNom; 
+				sortie.output(document, new FileOutputStream("sauvegarde/profils.xml"));
+	        }catch(Exception e){
+				System.out.println("Erreur d'enregistrement");
+			}
+		}
 	}
 	
 	public void charger(){
