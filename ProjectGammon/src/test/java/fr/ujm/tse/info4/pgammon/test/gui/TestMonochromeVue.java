82290ceae0b4d0b56ef79408bf851aa86d01cc3d
POTHELUNE Jean-Michel <pothelune.jean-michel@telecom-st-etienne.fr>
2012-12-19 21:20:22 +0100

    Refactor - Generalisation des JPanels de vues en MonochromeVue (not used yet)

---
+++ ProjectGammon/src/test/java/fr/ujm/tse/info4/pgammon/test/gui/TestMonochromeVue.java
@@ -0,0 +1,24 @@
+package fr.ujm.tse.info4.pgammon.test.gui;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+
+import fr.ujm.tse.info4.pgammon.gui.MonochromeVue;
+
+public class TestMonochromeVue {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Test Design");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(816,638);
+
+		MonochromeVue mv = new MonochromeVue();
+		
+		frame.setContentPane(mv);
+		mv.afficherTransition("aa", "bb");
+		
+		frame.setVisible(true);
+	}
+}
