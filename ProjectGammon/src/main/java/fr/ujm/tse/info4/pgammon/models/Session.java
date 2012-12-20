f505777330178f4c73306dfaee8de65b961c7362
bbonnetto <bbonnetto@gmail.com>
2012-12-20 10:29:23 +0100

    Revert "modification session mineur"
    This reverts commit e8b41ecd5197f2baefcfa57d3969bde696d2dfe4.

--- ProjectGammon/src/main/java/fr/ujm/tse/info4/pgammon/models/Session.java
+++ ProjectGammon/src/main/java/fr/ujm/tse/info4/pgammon/models/Session.java
@@ -56,13 +56,6 @@ public class Session
 		//TODO
 		throw new UnsupportedOperationException();
 	}
-	
-	public void finSession(Joueur joueurGagnantSession)
-	{
-		this.joueurGagnantSession = joueurGagnantSession;
-		finSession();
-	}
-	
 	private void finSession()
 	{
 		joueurGagnantSession.getStat().ajouterVictoire();
@@ -87,7 +80,6 @@ public class Session
 		else
 			partieEnCours.lancerNouvellePartie(couleurJoueurAnciennePartie);
 	}
-
 	
 	public void finPartie()
 	{
@@ -115,16 +107,6 @@ public class Session
 			return true;
 		}
 		return false;
-	}
-	
-	public Joueur meilleurJoueur()
-	{
-		if (scores.get(parametreSession.joueurBlanc) > scores.get(parametreSession.joueurNoir))
-				return parametreSession.joueurBlanc;
-		else if (scores.get(parametreSession.joueurBlanc) < scores.get(parametreSession.joueurNoir))
-			return parametreSession.joueurNoir;
-		else
-			return null;
 	}
 	
 	public void sauvegarder(Element racine)
